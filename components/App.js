cce2b21684a356c651ac193595bb8fd37b634316
Jonathan Hong <jonmhong@gmail.com>
2017-03-24 12:24:02 -0700

    Create App.js

---
+++ components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+	render() {
+		return (
+			<div>Hello World! This makes it a React app.</div>
+		)
+	}
+}
+
+export default App
